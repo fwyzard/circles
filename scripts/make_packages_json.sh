de517aac26fc6ee0e6274d1c66bbd24305a472fe
Andrea Bocci <andrea.bocci@cern.ch>
2021-03-04 18:47:16 +0100

    Add a script to automatically generate the packages.json group file

---
+++ scripts/make_packages_json.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+MACROS="DEFINE_FWK_MODULE DEFINE_FWK_INPUT_SOURCE DEFINE_FWK_VECTOR_INPUT_SOURCE"
+REGEX="\($(echo $MACROS | sed -e's/ \+/\\|/g')\)"
+
+echo '{'
+cd $CMSSW_RELEASE_BASE/src
+{
+  grep -r "^ *$REGEX" */*/plugins */*/src */*/interface | sed -e"s#\(\w\+\)/\(\w\+\)/.*: *$REGEX *( *\(.*::\)\?\(\w\+\) *).*#  \"\5|\": \"\1|\2\",#"
+  echo '  "PathStatusInserter|": "FWCore|Framework",'
+  echo '  "EndPathStatusInserter|": "FWCore|Framework",'
+  echo '  "TriggerResultInserter|": "FWCore|Framework",'
+} | sort -u
+echo '  "other|other": "other|other"'
+echo '}'
