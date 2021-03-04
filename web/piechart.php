4f913432cd1f8468c259531b0e065ec46d1ef55e
Patrick Gartung <gartung@fnal.gov>
2021-03-04 09:58:07 -0600

    Remove NO_SORT from glob so entries are sorted.

--- web/piechart.php
+++ web/piechart.php
@@ -45,7 +45,7 @@
       // does not support flag GLOB_BRACE
       function rglob($pattern, $flags = 0) {
           $files = glob($pattern, $flags);
-          foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+          foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR) as $dir) {
               $files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
           }
           return $files;
