cc07a5dc645e6691382e5828ec0bebf156fba875
Andrea Bocci <andrea.bocci@cern.ch>
2024-02-22 14:25:45 +0100

    Keep template modules and add idle category

--- scripts/make_packages_json.sh
+++ scripts/make_packages_json.sh
@@ -6,10 +6,11 @@ REGEX="\($(echo $MACROS | sed -e's/ \+/\\|/g')\)"
 echo '{'
 cd $CMSSW_RELEASE_BASE/src
 {
-  grep -r "^ *$REGEX" */*/plugins */*/src */*/interface | sed -e"s#\(\w\+\)/\(\w\+\)/.*: *$REGEX *( *\(.*::\)\?\(\w\+\) *).*#  \"\5|\": \"\1|\2\",#"
+  grep -r "^ *$REGEX" */*/plugins */*/src */*/interface | sed -e"s#\(\w\+\)/\(\w\+\)/.*: *$REGEX *( *\(.*::\)\?\([a-zA-Z0-9_<>:]\+\) *).*#  \"\5|\": \"\1|\2\",#"
   echo '  "PathStatusInserter|": "FWCore|Framework",'
   echo '  "EndPathStatusInserter|": "FWCore|Framework",'
   echo '  "TriggerResultInserter|": "FWCore|Framework",'
 } | sort -u
+echo '  "idle|idle": "idle",'
 echo '  "other|other": "other|other"'
 echo '}'
