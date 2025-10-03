78d551e8100b3524559d9b4762229e45b6aab8fd
Andrea Bocci <andrea.bocci@cern.ch>
2025-10-03 21:39:20 +0200

    Update packages for CMSSW_16_0_X_2025-10-03-1100
    Remove duplicate entries for the same module, and update the path of the modules
    that moved to a different package.

--- scripts/make_packages_json.sh
+++ scripts/make_packages_json.sh
@@ -1,7 +1,24 @@
 #! /bin/bash
 
+# Usage:
+#   ./make_packages_json.sh [-u web/groups/packages.json]
+#
+# Options:
+#   -u FILE     Start from the content of an existing JSON group file
+#
+# Find all framework modules declared in the plugins, src, and interface subdirectories
+# of all packages under $CMSSW_RELEASE_BASE/src, and print a JSON file suitable for the
+# groups of a circles piechart.
+# Optionally start from an existing JSON file, and print the resulting group file.
+
 MACROS="DEFINE_FWK_MODULE DEFINE_FWK_INPUT_SOURCE DEFINE_FWK_VECTOR_INPUT_SOURCE"
 REGEX="\($(echo $MACROS | sed -e's/ \+/\\|/g')\)"
+UPDATE=
+
+if [ "$1" == "-u" ] && [ -f "$2" ]; then
+  UPDATE=$(realpath "$2")
+  shift 2
+fi
 
 echo '{'
 cd $CMSSW_RELEASE_BASE/src
@@ -10,7 +27,8 @@ cd $CMSSW_RELEASE_BASE/src
   echo '  "PathStatusInserter|": "FWCore|Framework",'
   echo '  "EndPathStatusInserter|": "FWCore|Framework",'
   echo '  "TriggerResultInserter|": "FWCore|Framework",'
-} | sort -u
+  [ "$UPDATE" ] && cat "$UPDATE" | grep -v [{}] | grep -v -w 'idle\|other'
+} | sort -t'"' -k2,2 -s -u
 echo '  "idle|idle": "idle",'
 echo '  "other|other": "other|other"'
 echo '}'
