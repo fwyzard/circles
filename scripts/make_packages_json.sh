24dce01fd70bb5df1672eed0ac2467f1105b574a
Andrea Bocci <andrea.bocci@cern.ch>
2025-10-04 11:05:26 +0200

    Add EventSetup and alpaka-based modules to the packages-based groups (#45)
    Update packages for CMSSW_16_0_X_2025-10-03-1100.
    Remove duplicate entries for the same module, and update the path of the modules
    that moved to a different package.
    Add ESProducers, ESSources, and alpaka-based modules.
    
    Co-authored-by: Patrick Gartung <gartung@fnal.gov>

--- scripts/make_packages_json.sh
+++ scripts/make_packages_json.sh
@@ -1,16 +1,46 @@
 #! /bin/bash
 
-MACROS="DEFINE_FWK_MODULE DEFINE_FWK_INPUT_SOURCE DEFINE_FWK_VECTOR_INPUT_SOURCE"
+# Usage:
+#   ./make_packages_json.sh [-u web/groups/packages.json]
+#
+# Options:
+#   -u FILE     Start from the content of an existing JSON group file
+#
+# Find all framework modules declared in the plugins, src, and interface subdirectories
+# of all packages under $CMSSW_RELEASE_BASE/src, and print a JSON file suitable for the
+# groups of a circles piechart.
+# Optionally start from an existing JSON file, and print the resulting group file.
+
+MACROS="DEFINE_FWK_MODULE DEFINE_FWK_INPUT_SOURCE DEFINE_FWK_VECTOR_INPUT_SOURCE DEFINE_FWK_EVENTSETUP_MODULE DEFINE_FWK_EVENTSETUP_SOURCE"
 REGEX="\($(echo $MACROS | sed -e's/ \+/\\|/g')\)"
+
+ALPAKA_MACROS="DEFINE_FWK_ALPAKA_MODULE DEFINE_FWK_EVENTSETUP_ALPAKA_MODULE"
+ALPAKA_REGEX="\($(echo $ALPAKA_MACROS | sed -e's/ \+/\\|/g')\)"
+
+UPDATE=
+if [ "$1" == "-u" ] && [ -f "$2" ]; then
+  UPDATE=$(realpath "$2")
+  shift 2
+fi
 
 echo '{'
 cd $CMSSW_RELEASE_BASE/src
 {
-  grep -r "^ *$REGEX" */*/plugins */*/src */*/interface | sed -e"s#\(\w\+\)/\(\w\+\)/.*: *$REGEX *( *\(.*::\)\?\([a-zA-Z0-9_<>:]\+\) *).*#  \"\5|\": \"\1|\2\",#"
+  grep "^ *$REGEX\>" \
+    -r */*/plugins */*/src */*/interface \
+    --exclude MakerMacros.h \
+    --exclude ModuleFactory.h \
+    | sed -e"s#\(\w\+\)/\(\w\+\)/.*: *$REGEX *( *\(.*::\)\?\([a-zA-Z0-9_<>:]\+\) *).*#  \"\5|\": \"\1|\2\",#"
+  grep "^ *$ALPAKA_REGEX\>" \
+    -r */*/plugins/alpaka */*/src/alpaka */*/interface/alpaka \
+    --exclude MakerMacros.h \
+    --exclude ModuleFactory.h \
+    | sed -e"s#\(\w\+\)/\(\w\+\)/.*: *$ALPAKA_REGEX *( *\(.*::\)\?\([a-zA-Z0-9_<>:]\+\) *).*#  \"\5@alpaka|\": \"\1|\2\",#"
   echo '  "PathStatusInserter|": "FWCore|Framework",'
   echo '  "EndPathStatusInserter|": "FWCore|Framework",'
   echo '  "TriggerResultInserter|": "FWCore|Framework",'
-} | sort -u
+  [ "$UPDATE" ] && cat "$UPDATE" | grep -v [{}] | grep -v -w 'idle\|other'
+} | sort -t'"' -k2,2 -s -u
 echo '  "idle|idle": "idle",'
 echo '  "other|other": "other|other"'
 echo '}'
