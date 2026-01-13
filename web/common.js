6e365f2ae04c3ea70d852a02d7fcff9b4ead8ea7
Patrick Gartung <gartung@fnal.gov>
2026-01-13 20:55:54 +0100

    Check for presence of current.dataset.total.events before normalising

--- web/common.js
+++ web/common.js
@@ -685,7 +685,7 @@ function updateDataView() {
     console.log("Unassigned modules:");
     console.table(unassigned);
   }
-  if (config.resource.startsWith("time_") || config.resource.startsWith("mem_")) {
+  if (current.dataset.total.events) {
     normalise(current.data, current.dataset.total.events);
   }
 
@@ -794,7 +794,7 @@ $(document).ready(function () {
   circles.set("onGroupHover", function (hover) {
     if (hover.group) {
       tooltip.innerHTML = escape(hover.group.label) + "<br>" + hover.group.weight.toFixed(1) + " " + current.unit;
-      if ("events" in hover.group) {
+      if (hover.group.events) {
         tooltip.innerHTML += "<br>" + (hover.group.events * 100.).toFixed(1) + "% events";
       }
       if ("ratio" in hover.group) {
