5043dd86c898ab47ee874764ee3dd0841d3ded7e
Patrick Gartung <gartung@fnal.gov>
2026-01-16 05:29:36 -0600

    Normalise and show event fraction only for event-based metrics (#54)

--- web/common.js
+++ web/common.js
@@ -685,7 +685,9 @@ function updateDataView() {
     console.log("Unassigned modules:");
     console.table(unassigned);
   }
-  normalise(current.data, current.dataset.total.events);
+  if (current.dataset.total.events !== undefined) {
+    normalise(current.data, current.dataset.total.events);
+  }
 
   for (key in current.colours) {
     group = getGroup(key.split("|"));
@@ -792,7 +794,7 @@ $(document).ready(function () {
   circles.set("onGroupHover", function (hover) {
     if (hover.group) {
       tooltip.innerHTML = escape(hover.group.label) + "<br>" + hover.group.weight.toFixed(1) + " " + current.unit;
-      if ("events" in hover.group) {
+	  if (hover.group.events !== undefined) {
         tooltip.innerHTML += "<br>" + (hover.group.events * 100.).toFixed(1) + "% events";
       }
       if ("ratio" in hover.group) {
